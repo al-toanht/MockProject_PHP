7ef05080777cf3561aacdbbe1886821a25b18f35
toanht <toanht@allogical.com>
2021-11-03 17:34:31 +0700

    update: Update file App.php

--- App/App.php
+++ App/App.php
@@ -1,21 +1,27 @@
 <?php
 class App {
-    private $__controller, $__action,$__params,$__routes;
+    private $__controller, $__action,$__params,$__routes,$__db;
     static public $app;
     function __construct(){
         global $routes;
         self::$app = $this;
         $this->__routes= new Route();
+
         if(!empty($routes['default_controller'])){
             $this->__controller=$routes['default_controller'];
         }
+
         $this->__action='index';
         $this->__params=[];
-        $this->handleUrl();
-     
 
+        if(class_exists('DB')){
+        $dbOject= new DB();
+        $this->__db= $dbOject->db; 
+        }
+        $this->handleUrl();
     }
-    function getUrl(){
+
+    public function getUrl(){
         if(!empty($_SERVER['PATH_INFO'])){
             $url=$_SERVER['PATH_INFO'];
         }else{
@@ -23,12 +29,14 @@ class App {
         }
         return $url;
     }
+
     public function handleUrl(){
         $url = $this->getUrl();
         $url= $this->__routes->handleRoute($url);
         $urlArr=array_filter(explode('/',$url));
         $urlArr=array_values($urlArr);
         $urlCheck='';
+        //Xử lí route
         if(!empty($urlArr)){
             foreach( $urlArr as $key=>$item){
                 $urlCheck.=$item.'/';
@@ -61,6 +69,10 @@ class App {
             if(class_exists($this->__controller)){
                 $this->__controller = new $this->__controller();
                 unset($urlArr[0]);
+                
+                if(!empty($this->__db)){
+                    $this->__controller->db= $this->__db;
+                }
             }else{
                 $this->loadError();
             }
@@ -72,13 +84,16 @@ class App {
             $this->__action=$urlArr[1];
             unset($urlArr[1]);
         }
+
         $this->__params=array_values($urlArr);
+
         if(method_exists($this->__controller,$this->__action)){
             call_user_func_array([$this->__controller,$this->__action],$this->__params);
         }else{
             $this->loadError();
         }
     }
+    
     public function loadError($name='404',$data=[]){
         extract($data);
         require_once 'errors/'.$name.'.php';
