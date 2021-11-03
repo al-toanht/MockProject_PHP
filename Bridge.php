eaffcbb529abd9da08dbea5b6f910545ed4f16d9
toanht <toanht@allogical.com>
2021-11-03 17:29:31 +0700

    update: Update file Bridge.php

--- Bridge.php
+++ Bridge.php
@@ -1,5 +1,16 @@
 <?php
 define('_DIR_ROOT',__DIR__);
+//Xử lí http root
+if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on'){
+    $web_root= 'https://'. $_SERVER['HTTP_HOST'];
+}else{
+    $web_root= 'http://'. $_SERVER['HTTP_HOST'];
+}
+$folder = str_replace(strtolower($_SERVER['DOCUMENT_ROOT']),'',strtolower(_DIR_ROOT));
+$web_root= str_replace('\\','/',$web_root);
+define('_WEB_ROOT',$web_root);
+
+//Xử lí import các file trong folder configs
 $configs_dir = scandir('configs');
 if(!empty($configs_dir)){
     foreach($configs_dir as $item){
@@ -8,6 +19,7 @@ if(!empty($configs_dir)){
         }
     }
 }
+
 require_once "Core/Route.php"; // load route class  
 require_once "App/App.php"; // load app
 //Kiem tra config va load Database
@@ -17,8 +29,10 @@ if(!empty($config['database'])){
         require_once 'Core/Connection.php';
         require_once 'Core/QueryBuilder.php';
         require_once 'Core/Database.php';
+        require_once 'Core/DB.php';
     }
 }
-require_once 'Core/Model.php';
-require "Core/Controller.php"; //load BaseController
+
+require_once 'Core/Model.php'; //load BaseModels
+require_once "Core/Controller.php"; //load BaseController
 ?>
