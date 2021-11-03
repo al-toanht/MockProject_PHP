45b4e465245d9fab1b08ae09ae66cea51b6d4f54
toanht <toanht@allogical.com>
2021-11-03 17:40:08 +0700

    update: Update file CategoryModel

--- App/Models/CategoryModel.php
+++ App/Models/CategoryModel.php
@@ -6,14 +6,45 @@ class CategoryModel extends Model {
     function fieldFill(){
         return '*';
     }
+    function primaryKey(){
+        return 'id';
+    }
     
     public function getListCategory(){
-        $data= $this->db->table($this->tableFill())->where('id','>',5)->orWhere('id','=','2')->select($this->fieldFill())->get();
+        $data= $this->db->table('categories')->select()->get();
         return $data;
     }
-    public function getDetailCategory($categoryname){
-        $data= $this->db->table($this->tableFill())->where('id','>',2)->where('category_name','=',$categoryname)->select($this->fieldFill())->first();
+    // public function getDataLimit(){
+    //     $data = $this->db->table('categories')->select()->limit(3,1)->orderBy('id','DESC')->get();
+    //     return $data;
+    // }
+    public function getDetailCategory($id){
+        $data= $this->db->table('categories')->where('id','=',$id)->get();
         return $data;
+    }
+    public function getListnotCategory($id){
+        $data= $this->db->table('categories')->where('id','!=',$id)->get();
+        return $data;
+    }
+    public function getJoin(){
+        $data = $this->db->table('news')->join('categories','news.cate_id=categories.id')->get();
+        return $data;
+    }
+    public function createCategory($data){
+        $this->db->table('categories')->insert($data);
+        return $this;
+    }
+    public function getCategoryParent(){
+        $data= $this->db->table('categories')->where('parent_id','=',0)->select()->get();
+        return $data;
+    }
+    public function updateCategory($data,$id){
+        $this->db->table('categories')->where('id','=',$id)->update($data); 
+        return $this;
+    }
+    public function deleteCategory($id){
+        $this->db->table('categories')->where('id','=',$id)->delete();
+        return $this;
     }
    
 } 
