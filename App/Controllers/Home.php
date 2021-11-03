243a00118969baef33ac565e294877153f63dd32
toanht <toanht@allogical.com>
2021-11-03 17:44:16 +0700

    add: Add file home.php

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -1,7 +1,13 @@
 <?php
-class Home{
+class Home extends Controller{
+    private $categories;
+    public function __construct(){
+        $this->categories=$this->model('CategoryModel');
+    }
     public function index(){
-        echo "Trang chu ";
+        $listcategories= $this->categories->getListCategory();
+        $this->data['sub_content']['listcategories']= $listcategories;
+        $this->view('user/layouts/users_layout',$this->data);
     }
 } 
 ?>
